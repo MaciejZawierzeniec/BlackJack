e2440569e93c4a98c1bf14434bc8ee6271e72336
Maciej <33521816+User10500@users.noreply.github.com>
2019-11-15 01:01:17 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -20,7 +20,7 @@ class Card():
 class Deck:
 
     def __init__(self):
-        self.deck = []  # start with an empty list
+        self.deck = [] 
         for suit in suits:
             for rank in ranks:
                 self.deck.append(Card(suit, rank))
@@ -38,9 +38,9 @@ class Deck:
 class Hand:
 
     def __init__(self):
-        self.cards = []  # start with an empty list as we did in the Deck class
-        self.value = 0   # start with zero value
-        self.aces = 0    # add an attribute to keep track of aces
+        self.cards = []
+        self.value = 0
+        self.aces = 0
 
     def add_card(self,card):
         self.cards.append(card)
@@ -54,7 +54,7 @@ class Hand:
 class Chips:
 
     def __init__(self):
-        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.total = 100
         self.bet = 0
 
     def win_bet(self):
@@ -118,10 +118,7 @@ def push():
 
 
 while True:
-    # Print an opening statement
 
-
-    # Create & shuffle the deck, deal two cards to each player
     deck = Deck()
     deck.shuffle()
     player = Hand()
@@ -131,37 +128,28 @@ while True:
     dealer.add_card(deck.deal())
     dealer.add_card(deck.deal())
 
-    # Set up the Player's chips
     player_chips = Chips()
 
-    # Prompt the Player for their bet
     take_bet()
 
-    # Show cards (but keep one dealer card hidden)
     show_some(player,dealer)
 
-    while playing:  # recall this variable from our hit_or_stand function
+    while playing:
 
-        # Prompt for Player to Hit or Stand
         hit_or_stand(deck,player)
 
-        # Show cards (but keep one dealer card hidden)
         show_some(player,dealer)
 
-        # If player's hand exceeds 21, run player_busts() and break out of loop
         if player.value > 21:
             player_busts()
             break
 
 
-    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
         while dealer.value < 17:
             dealer.add_card(deck.deal())
 
-        # Show all cards
         show_all(player,dealer)
 
-        # Run different winning scenarios
         if dealer.value > player.value:
             player_busts()
             player_chips.lose_bet()
@@ -172,9 +160,7 @@ while True:
             print("Draw")
 
 
-    # Inform Player of their chips total
 
-    # Ask to play again
     again = input("Would you like to play again(y/n)? ")
     if again == "y":
         playing = True
